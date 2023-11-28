37ded3fc8d4f78e95c394b2aa17dc3bac7ac5a37
devxyn <johnlawrencehermo@gmail.com>
2023-11-28 15:57:50 +0800

    feat: added id number for everypokemon and fix: favicon

--- src/components/AllData.jsx
+++ src/components/AllData.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 const AllData = ({ data, imageUrl }) => {
+  console.log(data);
   return (
     <div className="flex flex-row flex-wrap items-center justify-center my-5 gap-10">
       {data.map((info, index) => (
@@ -10,8 +11,10 @@ const AllData = ({ data, imageUrl }) => {
             alt={info.name}
           />
 
-          <div className="flex justify-center items-center py-4 border-t-2 border-black bg-red-600 text-white ">
+          <div className="flex flex-col justify-center items-center py-4 border-t-2 border-black bg-red-600 text-white ">
             <h2 className="text-xl">{info.name.toUpperCase()}</h2>
+
+            <h2>ID: {index + 1}</h2>
           </div>
         </div>
       ))}
